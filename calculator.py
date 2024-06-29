1ec4434b23b92ddc35f2e59fcbba933e9a81fd00
spotlight3 <119120766+spotlight3@users.noreply.github.com>
2024-06-29 15:09:00 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -5,9 +5,9 @@
 # Asks user for 2 operands and 1 operator
 # Returns output of this operation
 
-a=input("Enter number 1 : ")
+a=int(input("Enter number 1 : "))
 o=input("Enter operator : ")
-b=input("Enter number 2 : ")
+b=int(input("Enter number 2 : "))
 
 if o[0] in [ '+','-','*','/' ]:
     if o[0] == '+':
@@ -17,7 +17,10 @@ if o[0] in [ '+','-','*','/' ]:
     elif o[0] == '*':
         out = a * b
     elif o[0] == '/':
-        out = a//b
+        if(b==0):
+            print("cannot divide")
+        else:    
+            out = a//b
     print("Output : ",out)
 else:
     print("Error : Invalid Operator")
